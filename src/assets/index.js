dcc7c14a1a67e95e33737b56bcb1b0bf89d4ab64
Sameer Faridi <s.faridi007@gmail.com>
2024-03-30 01:15:06 +0530

    add some assets for future

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,6 @@ import google from "./google.svg";
 import github from "./github.png";
 import grid from "./BottomGrid.svg";
 import gridMobile from "./gridMobile.svg";
-export { gradient, google, github, grid, gridMobile } ;
+import grid3d from "./3dGrid.jpg";
+import gridCurve from "./gridCurve.jpg";
+export {gridCurve ,gradient, google, github, grid, gridMobile, grid3d} ;
