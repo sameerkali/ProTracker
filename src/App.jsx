4f6d9b0ab26a3c4840c7e2f72bb80cf46db16ea0
Sameer Faridi <s.faridi007@gmail.com>
2024-03-30 00:04:24 +0530

    fix home

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import AddTodo from "./Components/AddTodo";
 import LogInForm from "./Components/LogInForm";
 import SignUpForm from "./Components/SignUpForm";
 import { getAuth } from "firebase/auth";
+import Profile from "./Components/profile";
 
 const auth = getAuth();
 
 const isLoggedIn = () => {
-  return !!auth.currentUser;
+  return auth.currentUser !== null;
 };
 
 const ProtectedRoute = ({ children }) => {
@@ -24,12 +25,12 @@ const ProtectedRoute = ({ children }) => {
 
   return children;
 };
-
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
       <Route path="/add" element={<ProtectedRoute><AddTodo /></ProtectedRoute>} />
+      <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
       <Route path="/login" element={<LogInForm />} />
       <Route path="/signup" element={<SignUpForm />} />
       <Route path="*" element={<NotFound />} />
