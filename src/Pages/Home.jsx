674f38350092869a78b599eb47dc2a3346231a27
Sameer Faridi <s.faridi007@gmail.com>
2024-03-30 15:49:52 +0530

    only a specific user can see there todos

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,23 +11,28 @@ import {
   doc
 } from "firebase/firestore";
 import Profile from "../Components/profile";
-import { gridMobile } from "../assets";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth();
 
 const Home = () => {
   const nameRef = useRef(null);
   const descriptionRef = useRef(null);
 
-  const handleResize = (ref) => {
+  const handleResize = ref => {
     const textarea = ref.current;
     textarea.style.height = "auto";
     textarea.style.height = `${textarea.scrollHeight}px`;
   };
 
   const [todos, setTodos] = useState([]);
+  // console.log("todos", todos[0]?.data?.userUID)
+  // todos.map((t) => {
+  //   console.log("map all id's",t?.data?.userUID)
+  // })
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "todos"), (snapshot) => {
-      setTodos(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })));
+    const unsubscribe = onSnapshot(collection(db, "todos"), snapshot => {
+      setTodos(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })));
     });
 
     return () => unsubscribe();
@@ -37,27 +42,27 @@ const Home = () => {
     const name = nameRef.current.value;
     const description = descriptionRef.current.value;
     const currentDate = new Date();
-  
+
     if (name.trim() !== "") {
       await addDoc(collection(db, "todos"), {
         name: name.trim(),
         description,
         isCompleted: false,
         priority: ["low", "medium", "high"],
-        label: '', // Assuming label is a string, you can assign it here
-        date: currentDate
+        label: "", // Assuming label is a string, you can assign it here
+        date: currentDate,
+        userUID: user.uid
       });
       nameRef.current.value = "";
       descriptionRef.current.value = "";
     }
   };
-  
 
   const handleEditTodo = async (id, newData) => {
     await updateDoc(doc(db, "todos", id), newData);
   };
 
-  const handleDeleteTodo = async (id) => {
+  const handleDeleteTodo = async id => {
     await deleteDoc(doc(db, "todos", id));
   };
 
@@ -65,55 +70,90 @@ const Home = () => {
     await updateDoc(doc(db, "todos", id), { isCompleted: !isCompleted });
   };
 
+  // user logic
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setUser(user);
+      setLoading(false);
+      if (user) {
+        localStorage.setItem("user", JSON.stringify(user));
+      } else {
+        localStorage.removeItem("user");
+      }
+    });
+
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+
+    return () => unsubscribe();
+  }, []);
+  if (loading) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <span className="loading loading-dots loading-lg" />
+      </div>
+    );
+  }
+
+  console.log("user in home page:", user.uid);
+  // user logic
+
   return (
     <div className="">
       <Profile />
       <div className="flex justify-center">
         <ul className="w-[20rem] sm:w-[50rem]">
-          {todos.map((todo, index) => (
-            <li
-              key={todo.id}
-              className="flex items-center justify-between border-b border-gray-700 py-2"
-            >
-              <div className="flex gap-10 items-center">
-                <input
-                  type="checkbox"
-                  value={todo.data.isCompleted}
-                  checked={todo.data.isCompleted}
-                  onChange={() =>
-                    handleCheckboxChange(todo.id, todo.data.isCompleted)
-                  }
-                  className="checkbox theme-controller"
-                />
-                <div className="">
-                <div
-                  className={
-                    "text-[15px] " + (todo.data.isCompleted ? "text-green-500" : "")
-                  }
-                >
-                  {todo.data.name}
-                </div>
-                <div 
-                 className={
-                    "text-[12px] " + (todo.data.isCompleted ? "text-green-500" : "")
-                  }
-                >{todo.data.description}</div>
-                </div>
-              </div>
-              <div className="flex">
-                <CiEdit
-                  onClick={() => {
-                    // Implement edit functionality
-                  }}
-                  className="text-gray-400 cursor-pointer mx-1 hover:text-white text-xl"
-                />
-                <AiOutlineDelete
-                  onClick={() => handleDeleteTodo(todo.id)}
-                  className="text-gray-400 cursor-pointer mx-3 hover:text-white text-xl"
-                />
-              </div>
-            </li>
-          ))}
+          {todos.map((todo, index) =>
+            <div key={todo.id}>
+              {todo.data.userUID === user.uid &&
+                <li className="flex items-center justify-between border-b border-gray-700 py-2">
+                  <div className="flex gap-10 items-center ">
+                    <input
+                      type="checkbox"
+                      value={todo.data.isCompleted}
+                      checked={todo.data.isCompleted}
+                      onChange={() =>
+                        handleCheckboxChange(todo.id, todo.data.isCompleted)}
+                      className="checkbox checkbox-success theme-controller"
+                    />
+                    <div className="">
+                      <div
+                        className={
+                          "text-[15px] " +
+                          (todo.data.isCompleted ? "text-[#00A96D]" : "")
+                        }
+                      >
+                        {todo.data.name}
+                      </div>
+                      <div
+                        className={
+                          "text-[12px] " +
+                          (todo.data.isCompleted ? "text-[#00A96D]" : "")
+                        }
+                      >
+                        {todo.data.description}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <CiEdit
+                      onClick={() => {
+                        // Implement edit functionality
+                      }}
+                      className="text-gray-400 cursor-pointer mx-1 hover:text-green-400 text-xl"
+                    />
+                    <AiOutlineDelete
+                      onClick={() => handleDeleteTodo(todo.id)}
+                      className="text-gray-400 cursor-pointer mx-3 hover:text-red-300 text-xl"
+                    />
+                  </div>
+                </li>}
+            </div>
+          )}
         </ul>
       </div>
       <div className="my-10 flex items-center justify-center">
